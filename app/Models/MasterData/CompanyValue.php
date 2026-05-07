5182481116e01ceebed3fcf2a5c7387205534776
superevilstockholm <balilombok11@gmail.com>
2026-05-07 16:02:34 +0700

    Created company values table

---
+++ app/Models/MasterData/CompanyValue.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\MasterData;
+
+use Illuminate\Database\Eloquent\Model;
+
+// Attributes
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+
+#[Fillable(['title', 'description', 'order'])]
+class CompanyValue extends Model
+{
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'order' => 'integer',
+        ];
+    }
+}
