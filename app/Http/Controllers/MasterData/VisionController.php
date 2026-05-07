5910c862b8a8beab02df3ee5e6b72a24b5812502
superevilstockholm <balilombok11@gmail.com>
2026-05-07 14:04:57 +0700

    Created create and store method in master data/vision controller also created store request

--- app/Http/Controllers/MasterData/VisionController.php
+++ app/Http/Controllers/MasterData/VisionController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers\MasterData;
 use Carbon\Carbon;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 
 // Models
 use App\Models\MasterData\Vision;
 
 // Requests
 use App\Http\Requests\MasterData\Vision\IndexRequest;
+use App\Http\Requests\MasterData\Vision\StoreRequest;
 
 class VisionController extends Controller
 {
@@ -51,17 +54,29 @@ class VisionController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        $allowedMaxOrder = Vision::count() + 1;
+        return view('pages.dashboard.master-data.vision.index', [
+            'allowedMaxOrder' => $allowedMaxOrder,
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request): RedirectResponse
     {
-        //
+        $validated = $request->validated();
+
+        DB::transaction(function () use ($validated) {
+            Vision::where('order', '>=', $validated['order'])
+                ->lockForUpdate()
+                ->increment('order');
+            Vision::create($validated);
+        });
+
+        return redirect()->route('dashboard.master-data.visions.index')->with('success', 'Berhasil menambahkan visi.');
     }
 
     /**
