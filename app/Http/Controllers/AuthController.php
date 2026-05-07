9b5dc5c140292d097fd71fd22e67a63c5118cedb
superevilstockholm <balilombok11@gmail.com>
2026-05-07 12:35:55 +0700

    Created logout attempt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\View\View;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
 
@@ -27,5 +28,15 @@ class AuthController extends Controller
         $request->session()->regenerate();
 
         return redirect()->route('dashboard.index')->with('success', 'Berhail masuk.');
+    }
+
+    public function logout_attempt(Request $request): RedirectResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login.view')->with('success', 'Berhasil keluar.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,8 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/login', [AuthController::class, 'login_view'])->name('login.view');
 Route::post('/login', [AuthController::class, 'login_attempt'])->name('login.attempt');
+
+// Protected
+Route::middleware(['auth'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout_attempt'])->name('logout.attempt');
+});
