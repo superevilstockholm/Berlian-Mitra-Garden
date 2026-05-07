997f930297c00beb12ac520811a0584ef46a617e
superevilstockholm <balilombok11@gmail.com>
2026-05-07 13:47:23 +0700

    Add breadcrumb to dashboard layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,6 +4,51 @@
     <x-topbar></x-topbar>
     <div class="pc-container">
         <div class="pc-content">
+            <div class="row mb-4">
+                <div class="col">
+                    @php
+                        $segments = collect(request()->segments());
+                        $category = $segments->get(1);
+                        $item     = $segments->get(2);
+                        $subitem  = $segments->get(3);
+                        $isDashboardOnly = request()->is('dashboard');
+                    @endphp
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb bg-transparent mb-0">
+                            <li class="breadcrumb-item {{ $isDashboardOnly ? 'active' : 'fw-medium' }}"
+                                @if($isDashboardOnly) aria-current="page" @endif>
+                                @if($isDashboardOnly)
+                                    Dashboard
+                                @else
+                                    <a href="{{ url('dashboard') }}">Dashboard</a>
+                                @endif
+                            </li>
+                            @if($item)
+                                @php
+                                    $itemLabel = ucwords(str_replace('-', ' ', $item));
+                                    $itemUrl   = url("dashboard/$category/$item");
+                                @endphp
+                                <li class="breadcrumb-item {{ $subitem ? 'fw-medium' : 'active' }}"
+                                    @if(!$subitem) aria-current="page" @endif>
+                                    @if($subitem)
+                                        <a href="{{ $itemUrl }}">{{ $itemLabel }}</a>
+                                    @else
+                                        {{ $itemLabel }}
+                                    @endif
+                                </li>
+                            @endif
+                            @if($subitem)
+                                @php
+                                    $subLabel = ucwords(str_replace('-', ' ', $subitem));
+                                @endphp
+                                <li class="breadcrumb-item active" aria-current="page">
+                                    {{ $subLabel }}
+                                </li>
+                            @endif
+                        </ol>
+                    </nav>
+                </div>
+            </div>
             @yield('content')
         </div>
     </div>
